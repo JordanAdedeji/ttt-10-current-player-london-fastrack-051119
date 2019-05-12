e149262814d11afafe0c1d01661e42b546d72d0e
Jordan Adedeji <jordan_adedeji@outlook.com>
2019-05-12 11:14:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 === 0
+  if turn_count(board) % 2 == 0
     return "X"
-  else turn_count(board) % 2 === 1
+  else turn_count(board) % 2 == 1
     return "O"
   else 
 end
